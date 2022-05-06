307891cda5d2d3c78c264e8d0e2856f0a0deb43d
Hameedullah Khan <hameed@sudoconsultants.com>
2022-05-07 00:33:30 +0400

    fix: Fix local variables

--- locals.tf
+++ locals.tf
@@ -1,11 +1,13 @@
 locals {
   create_kms_key = var.kms_key_arn == "" ? true : false
-  kms_key_arn    = coalesce(var.kms_key_arn, resource.aws_kms_key.arn)
+  kms_key_arn    = coalesce(var.kms_key_arn, aws_kms_key.kms_key[0].arn)
 
-  name = var.name == "sudo-backup" ? "sudo-backup-${random_sting.random.result}" : var.name
+  name = var.name == "sudo-backup" ? "sudo-backup-${random_string.random.result}" : var.name
+
 }
 
 resource "random_string" "random" {
-  length           = 6
-  special          = false
+  length  = 6
+  special = false
+  upper = false
 }
